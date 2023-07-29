c215efaf2134149fd3638e0687d5ac9ce108009a
bblueberries <tata10433@gmail.com>
2023-07-29 11:23:51 +0700

    style : groupDeclare page & make groupImage folder

--- src/pages/group.tsx
+++ src/pages/group.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import groupSnake from "../../public/group-snake.jpeg";
+import groupSnake from "../../public/groupImages/group-snake.jpeg";
 import Head from "next/head";
 
 type Props = {};
@@ -14,16 +14,18 @@ export default function group({}: Props) {
         &nbsp;
       </div>
       <div className="flex flex-col justify-center items-center  ">
-        <h1 className=" font-IBMPlexSansThai font-bold">
+        <h1 className=" font-IBMPlexSansThai font-bold text-[32px]">
           คุณได้รับเลือกให้เข้า
         </h1>
 
         <Image
           src={groupSnake}
           alt="groupImage"
-          className=" w-3/12 rounded-xl"
+          className="w-[230px] sm:w-6/12 md:w-5/12 lg:w-5/12 xl:w-3/12 rounded-xl mt-4 shadow-md shadow-gray-300"
         />
-        <h3 className="font-salapao ">กลุ่มอสรพิษวารี</h3>
+        <h3 className="font-salapao font-bold sm:text-[20px] md:text-[25px] mt-4 ">
+          กลุ่มอสรพิษวารี
+        </h3>
       </div>
     </div>
   );
