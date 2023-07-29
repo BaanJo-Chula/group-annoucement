2ab4106d4da038dc867e668844537bb4bd916d09
bblueberries <tata10433@gmail.com>
2023-07-29 13:57:59 +0700

    style : style group declare page

--- src/pages/group.tsx
+++ src/pages/group.tsx
@@ -7,26 +7,28 @@ type Props = {};
 
 export default function group({}: Props) {
   return (
-    <div>
-      <div className="bg-blue-100 ">
+    <>
+      <div className="bg-blue-200 absolute top-0 left-0 w-screen">
         &nbsp;
         <br />
         &nbsp;
       </div>
-      <div className="flex flex-col justify-center items-center mt-3 ">
-        <h1 className=" font-IBMPlexSansThai font-bold text-[26px] sm:text-[28px] md:text-[32px]">
-          คุณได้รับเลือกให้เข้า
-        </h1>
+      <div className="flex flex-col h-screen pb-32">
+        <div className="flex flex-col justify-center items-center flex-grow">
+          <h1 className=" font-IBMPlexSansThai font-bold text-[26px] sm:text-[28px] md:text-[32px]">
+            คุณได้รับเลือกให้เข้า
+          </h1>
 
-        <Image
-          src={groupSnake}
-          alt="groupImage"
-          className="w-[230px] sm:w-6/12 md:w-5/12 lg:w-5/12 xl:w-3/12 rounded-xl mt-3 shadow-md shadow-gray-300"
-        />
-        <h3 className="font-salapao font-bold sm:text-[20px] md:text-[25px] mt-4 ">
-          กลุ่มอสรพิษวารี
-        </h3>
+          <Image
+            src={groupSnake}
+            alt="groupImage"
+            className="w-[230px] sm:w-5/12 md:w-4/12 lg:w-3/12 xl:w-2/12 rounded-xl mt-3 shadow-md shadow-gray-300"
+          />
+          <h3 className="font-salapao font-bold sm:text-[20px] md:text-[25px] mt-4 ">
+            กลุ่มอสรพิษวารี
+          </h3>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
