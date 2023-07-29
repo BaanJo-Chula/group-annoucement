fdb8de1995caba3585da44a578af37bf48df244f
bblueberries <tata10433@gmail.com>
2023-07-29 23:28:34 +0700

    fix(Footer): edit code style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,22 @@
-import React from "react";
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 export default function Footer() {
-  const handleLinkClick = () => {
-    window.open("https://www.instagram.com/baanjochula/", "_blank");
-  };
   return (
-    <div className="bg-red-500 h-32 w-screen absolute bottom-0 ">
+    <div className="bg-red-500 h-32 w-full absolute bottom-0 ">
       <section className=" font-salapao text-white flex flex-col items-center justify-center h-full">
-        <p className="font-[16px] justify-center w-auto">
-          รับเพื่อนก้าวใหม่ 2023
-        </p>
-        <div
-          className="flex items-center hover:bg-gray-700 hover:bg-opacity-10 p-1 rounded-md cursor-pointer"
-          onClick={handleLinkClick}
+        <p className=" w-auto">รับเพื่อนก้าวใหม่ 2023</p>
+
+        <Link
+          href="https://www.instagram.com/baanjochula/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <Icon icon={"mdi:instagram"} width={18} />
-          <p className="font-[14px] ml-1">baanjochula</p>
-        </div>
+          <div className="flex items-center hover:bg-gray-700 hover:bg-opacity-10 p-1 rounded-md cursor-pointer">
+            <Icon icon="mdi:instagram" width={18} />
+            <p className=" text-sm ml-1">baanjochula</p>
+          </div>
+        </Link>
       </section>
     </div>
   );
