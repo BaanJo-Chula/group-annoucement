87813122e2f728c72eee7d75b44e71a5f4fce28b
Poonpipob "Leon" Kunlayanathee <42491719+shalluv@users.noreply.github.com>
2023-07-30 23:34:44 +0700

    Chore/add prettier (#10)
    * feat: prettier
    
    * feat: format script
    
    * style: format codes

--- src/fonts/index.tsx
+++ src/fonts/index.tsx
@@ -1,33 +1,33 @@
-import localFont from "next/font/local";
-import { IBM_Plex_Sans_Thai } from "next/font/google";
+import localFont from 'next/font/local';
+import { IBM_Plex_Sans_Thai } from 'next/font/google';
 export const salapao = localFont({
   src: [
     {
-      path: "./salapaoFonts/MN-SALAPAO.ttf",
-      weight: "normal",
-      style: "normal",
+      path: './salapaoFonts/MN-SALAPAO.ttf',
+      weight: 'normal',
+      style: 'normal',
     },
     {
-      path: "./salapaoFonts/MN-SALAPAO-Italic.ttf",
-      weight: "normal",
-      style: "italic",
+      path: './salapaoFonts/MN-SALAPAO-Italic.ttf',
+      weight: 'normal',
+      style: 'italic',
     },
     {
-      path: "./salapaoFonts/MN-SALAPAO-Bold.ttf",
-      weight: "bold",
-      style: "normal",
+      path: './salapaoFonts/MN-SALAPAO-Bold.ttf',
+      weight: 'bold',
+      style: 'normal',
     },
     {
-      path: "./salapaoFonts/MN-SALAPAO-Bold-Italic.ttf",
-      weight: "bold",
-      style: "italic",
+      path: './salapaoFonts/MN-SALAPAO-Bold-Italic.ttf',
+      weight: 'bold',
+      style: 'italic',
     },
   ],
-  variable: "--font-salapao ",
+  variable: '--font-salapao ',
 });
 
 export const ibmthai = IBM_Plex_Sans_Thai({
-  subsets: ["thai"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  variable: "--font-ibmthai",
+  subsets: ['thai'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  variable: '--font-ibmthai',
 });
