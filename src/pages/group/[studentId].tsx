4840d44069c180834d674766f17a244f51c44269
bblueberries <tata10433@gmail.com>
2023-07-31 12:45:32 +0700

    feat: add bg and noImage image

--- src/pages/group/[studentId].tsx
+++ src/pages/group/[studentId].tsx
@@ -11,8 +11,8 @@ import { useRouter } from 'next/router';
 
 import { Icon } from '@iconify/react';
 import data from '../../constants/data.json';
-
-
+import desktopBg from '#/desktop-bg.svg';
+import noImage from '#/noImage.jpeg';
 export default function group() {
   
   const router = useRouter();
@@ -35,11 +35,15 @@ export default function group() {
   let imgAlt = groupName[group] + elementalName[element] || "";
 
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center pb-32">
+    <div
+    className="flex min-h-screen w-full flex-col items-center justify-center bg-cover bg-fixed bg-no-repeat"
+    style={{ backgroundImage: `url(${desktopBg.src})` }}
+  >
+    <div className="relative flex min-h-screen flex-col items-center justify-center pb-32 w-screen">
       <Link href="/">
         <Icon
           icon="ic:arrow-back"
-          className="absolute left-8 top-8 text-3xl text-red"
+          className="absolute left-6 top-14 text-3xl text-red"
         />
       </Link>
       <div className="flex flex-col items-center justify-center gap-6">
@@ -47,11 +51,11 @@ export default function group() {
           คุณได้รับเลือกให้เข้า
         </h1>
         <div className="relative aspect-square w-64 overflow-hidden rounded-xl shadow-xl">
-          <Image src={frameSrc} fill alt="" className="absolute z-10" />
-          <Image src={imgSrc} alt={imgAlt} fill />
+          <Image src={frameSrc||noImage} fill alt="" className="absolute z-10" />
+          <Image src={imgSrc||noImage} alt={imgAlt} fill />
         </div>
         <h3 className="font-salapao text-xl font-bold">{imgAlt}</h3>
       </div>
-    </div>
+    </div></div>
   );
 }
