5469db2a12a525f70093ee7ce364ff2cc8886dc4
Fall <surin123.james@gmail.com>
2023-07-31 08:00:09 +0700

    chore: format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,24 @@ import logo from '#/kungjo-panda.jpg';
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-fixed bg-cover bg-no-repeat min-h-screen w-full items-center justify-center" style={{backgroundImage: `url(${desktopBg.src})`}}>
-      <div className="relative flex h-52 sm:h-64 w-4/6 max-w-lg max-h-96 flex-col items-center justify-center rounded-3xl bg-red border-4 border-solid border-red/20">
-        <div className="absolute -top-14 sm:-top-16 h-28 sm:h-32 w-28 sm:w-32 rounded-full bg-cover" style={{backgroundImage: `url(${logo.src})`}}></div>
-        <div className="flex flex-col space-y-2 h-1/3 w-5/6 min-w-fit font-salapao text-white text-xl sm:text-2xl sm:text-center">
+    <div
+      className="flex min-h-screen w-full flex-col items-center justify-center bg-cover bg-fixed bg-no-repeat"
+      style={{ backgroundImage: `url(${desktopBg.src})` }}
+    >
+      <div className="relative flex h-52 max-h-96 w-4/6 max-w-lg flex-col items-center justify-center rounded-3xl border-4 border-solid border-red/20 bg-red sm:h-64">
+        <div
+          className="absolute -top-14 h-28 w-28 rounded-full bg-cover sm:-top-16 sm:h-32 sm:w-32"
+          style={{ backgroundImage: `url(${logo.src})` }}
+        ></div>
+        <div className="flex h-1/3 w-5/6 min-w-fit flex-col space-y-2 font-salapao text-xl text-white sm:text-center sm:text-2xl">
           <div>เลขรหัสนิสิต</div>
-          <input className="h-1/3 w-full rounded-xl sm:rounded-2xl bg-white/30 text-black text-xl indent-2 sm:indent-3" type="text" placeholder='66XXXXXXXX'></input>
+          <input
+            className="h-1/3 w-full rounded-xl bg-white/30 indent-2 text-xl text-black sm:rounded-2xl sm:indent-3"
+            type="text"
+            placeholder="66XXXXXXXX"
+          ></input>
         </div>
-        <div className="absolute flex justify-center items-center bottom-5 sm:bottom-8 h-1/6 w-1/4 min-w-fit rounded-lg font-salapao bg-white text-black text-xl sm:text-2xl">
+        <div className="absolute bottom-5 flex h-1/6 w-1/4 min-w-fit items-center justify-center rounded-lg bg-white font-salapao text-xl text-black sm:bottom-8 sm:text-2xl">
           login
         </div>
       </div>
